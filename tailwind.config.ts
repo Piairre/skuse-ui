51a63549567ed3296f8dc5eb5d2927e236925eab
Pierre Iaccarino <pierre-iaccarino@hotmail.fr>
2024-12-22 18:56:35 +0100

    fix: rename tailwind config to ts ext

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
+
 export default {
     darkMode: ["class"],
     content: [
@@ -78,4 +79,4 @@ export default {
   		}
   	}
   }
-}
+} satisfies Config
